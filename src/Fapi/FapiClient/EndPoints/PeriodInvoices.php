c785177d139dda571e020a4c91e7ab89ce4bfb5a
Jiří Slischka <slischkaj@gmail.com>
2018-12-02 20:24:58 +0100

    Added internal period invoices

---
+++ src/Fapi/FapiClient/EndPoints/PeriodInvoices.php
@@ -0,0 +1,29 @@
+<?php
+declare(strict_types = 1);
+
+namespace Fapi\FapiClient\EndPoints;
+
+use Fapi\FapiClient\EndPoints\Traits\Create;
+use Fapi\FapiClient\EndPoints\Traits\Delete;
+use Fapi\FapiClient\EndPoints\Traits\Find;
+use Fapi\FapiClient\EndPoints\Traits\FindAll;
+use Fapi\FapiClient\EndPoints\Traits\Update;
+use Fapi\FapiClient\Rest\FapiRestClient;
+
+final class PeriodInvoices
+{
+
+	use FindAll;
+	use Find;
+	use Create;
+	use Update;
+	use Delete;
+
+	public function __construct(FapiRestClient $client)
+	{
+		$this->client = $client;
+		$this->path = '/periodic-invoices';
+		$this->resources = 'periodic_invoices';
+	}
+
+}
